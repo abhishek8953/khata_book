3d41db9ed9805a9d1b3ec3958a202832bc3b3411
abhishek8953 <rajantiwariat@gmail.com>
2026-01-09 13:08:47 +0530

    proxy chanded to real apis

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://e-book-nu-lime.vercel.app"
+    process.env.FRONTEND_URL
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
